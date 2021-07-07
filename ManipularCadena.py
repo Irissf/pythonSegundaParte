fc08a27630ba538222fdc7483a6b63448b39e409
Irissf <iris.seijo@gmail.com>
2021-07-07 10:07:13 +0200

    cadenas

--- ManipularCadena.py
+++ ManipularCadena.py
@@ -1,3 +1,14 @@
 nombreDeUsuario = input("Introduce el nobre de usuario: ")
 
-print(nombreDeUsuario)
+#minusculas
+print(nombreDeUsuario.lower())
+#mayusculas
+print(nombreDeUsuario.upper())
+#primera letra en mayúscula
+print(nombreDeUsuario.capitalize())
+
+edad = input("Introduce la edad: ")
+#saber si es un dato numérico, devuelve true o false
+print(edad.isdigit())
+if (edad.isdigit() and int(edad)<18):
+    print("Es menor de edad")
